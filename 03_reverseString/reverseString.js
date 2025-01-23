4037719f7ddd3efd6f3d163222e4b1ca36df7ae1
urbanIntrovert <trefoil72@gmail.com>
2025-01-23 22:03:08 +0000

    feat: Created a function to take a string and reverse the characters

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinString = reverseArray.join("");
+    return joinString;
 };
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
