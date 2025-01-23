d1a6aa449e955998f3a3a9c74a75b31d8c8efa72
urbanIntrovert <trefoil72@gmail.com>
2025-01-23 23:33:21 +0000

    Feat: Implement function to remove specified value from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== remove) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
