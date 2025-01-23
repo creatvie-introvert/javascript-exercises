7ef3ced45a1743da8e49bcaaa6afc625e29ca6a2
urbanIntrovert <trefoil72@gmail.com>
2025-01-23 20:04:22 +0000

    feat: all tests have passed. added if statement to output 'ERROR' for negative numbers.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,26 @@
 const repeatString = function(string, num) {
     let result = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
     for (let i = 0; i < num; i ++) {
         result += string;
     } 
+
     return result
 };
 
+const number = Math.floor(Math.random() * 1000);
+
 console.log(repeatString('hey', 3));
 console.log(repeatString('hello', 10));
+console.log(repeatString('hi', 1));
+console.log(repeatString('bye', 0));
+console.log(repeatString('goodbye', -1));
+console.log(repeatString('odin', number));
+console.log(repeatString('', 10));
 
 // Do not edit below this line
 module.exports = repeatString;
