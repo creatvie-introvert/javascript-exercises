f8785cf8084c9cce3f07406dad79bd03e11df348
urbanIntrovert <trefoil72@gmail.com>
2025-01-24 00:24:33 +0000

    test: Add remaining function calls and verify all tests pass
    Added additional function calls to validate the removeFromArray function.
    
    Confirmed all test cases, including multiple removal scenarios, passed successfully.
    
    Ensured the function handles dynamic inputs as expected.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -6,6 +6,13 @@ const removeFromArray = function(array, ...remove) {
 
 removeFromArray([1, 2, 3, 4], 3);
 removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 4], 2);
+removeFromArray([1, 2, 2, 4], 7, "tacos");
+removeFromArray([1, 2, 2, 4], 7, 2);
+removeFromArray([1, 2, 2, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
