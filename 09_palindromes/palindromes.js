8263d1aec0d5ff6cb373b952fac4219d3702ca8f
urbanIntrovert <trefoil72@gmail.com>
2025-01-31 10:40:12 +0000

    fix: update palindrome function to ignore punctuation and spaces
    - Added preprocessing to remove non-alphanumeric characters.
    - Converted input string to lowercase for case insensitivity.
    - Ensured the function correctly checks palindromes in phrases with spaces and punctuation.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    let left = 0;
+    let right = cleanedStr.length - 1;
+
+    while (left < right) {
+        if (cleanedStr[left] !== cleanedStr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
+
+palindromes('racecar');
+palindromes('racecar!');
+palindromes('A car, a man, a maraca.');
+palindromes('Animal loots foliated detail of stool lamina.');
+palindromes('ZZZZ car, a man, a maracaz.');
+palindromes('rac3e3car');
+palindromes('r3ace3car');
 
 // Do not edit below this line
 module.exports = palindromes;
