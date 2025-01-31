1e41baae98c33c9191ac3e4bbc0dc7152cc81c45
urbanIntrovert <trefoil72@gmail.com>
2025-01-31 11:23:07 +0000

    feat: add Fibonacci function with input validation
    - Implemented a recursive Fibonacci function.
    - Ensured function correctly handles 0-based indexing.
    - Allowed numeric string inputs by parsing them as integers.
    - Added validation to return `"OOPS"` for negative numbers.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n, 10);
 
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n < 0) return "OOPS";
+
+    return fibonacci(n - 1) + fibonacci(n-2);
 };
+
+fibonacci(4);
+fibonacci(6);
+fibonacci(10)
+fibonacci(15);
+fibonacci(25);
+fibonacci(0);
+fibonacci(-25);
+fibonacci("0");
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 
 // Do not edit below this line
 module.exports = fibonacci;
